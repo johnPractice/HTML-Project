b823edafc0dfb0fa463c521de59649a3a6a9c7d0
Reza Mousavian <rezamosavian1014@gmail.com>
2020-12-16 10:00:07 -0800

    add scroll animation

---
+++ scroll-Animation/script.js
@@ -0,0 +1,12 @@
+const boxes = document.querySelectorAll('div.box');
+window.addEventListener('scroll', checkBoxes);
+
+function checkBoxes() {
+    console.log('hi')
+    const triggerBottom = window.innerHeight / 5 * 4;
+    boxes.forEach(box => {
+        const topBox = box.getBoundingClientRect().top;
+        if (topBox < triggerBottom) box.classList.add('show');
+        else box.classList.remove('show');
+    });
+}
